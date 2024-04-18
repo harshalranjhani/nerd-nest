89469786e85743857dd51d1564332ff880b9745a
Harshal Ranjhani <harshalranjhani@Harshals-MacBook-Air.local>
2024-04-18 19:22:35 +0530

    add user settings page

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -39,7 +39,7 @@ export function UserMenu({ user }: UserMenuProps) {
     <div className="flex items-center justify-between">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="pl-0">
+          <Button variant="ghost" className="pl-0 hover:bg-transparent">
             {user?.user_metadata.avatar_url ? (
               <Image
                 height={60}
@@ -61,24 +61,13 @@ export function UserMenu({ user }: UserMenuProps) {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent sideOffset={8} align="start" className="w-[180px]">
-          <DropdownMenuItem className="flex-col items-start">
-            <div className="text-xs font-medium">
-              {user?.user_metadata.name}
-            </div>
-            <div className="text-xs text-zinc-500">{user?.email}</div>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem asChild>
-            <a
-              href="https://vercel.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline-flex w-full items-center justify-between text-xs"
-            >
-              Vercel Homepage
-              <IconExternalLink className="ml-auto h-3 w-3" />
+          <DropdownMenuItem asChild className="flex-col items-start">
+            <a href={`/user/settings/${user.id}`}>
+              <div className="text-xs font-medium">Logged in as</div>
+              <div className="text-xs text-zinc-500">{user?.email}</div>
             </a>
           </DropdownMenuItem>
+          <DropdownMenuSeparator />
           <DropdownMenuItem onClick={signOut} className="text-xs">
             Log Out
           </DropdownMenuItem>
