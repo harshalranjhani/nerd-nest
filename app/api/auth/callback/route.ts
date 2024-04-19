b2301a297d1c79a4a84978fda36a0e39cb3d7d9f
Harshal Ranjhani <harshalranjhani@Harshals-MacBook-Air.local>
2024-04-19 13:39:18 +0530

    fix: user insertion

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -4,9 +4,6 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the Auth Helpers package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
 
@@ -16,22 +13,37 @@ export async function GET(request: Request) {
       cookies: () => cookieStore
     })
     const session = await supabase.auth.exchangeCodeForSession(code)
-    // insert the user into the users table
-    // await supabase.from('users').insert([{ id: session.user.id }])
     const supabase_user = session?.data?.session?.user
-    await supabase
-      .from('users')
-      .upsert({
-        user_id: supabase_user?.identities[0]?.user_id,
-        email: supabase_user?.email,
-        role: supabase_user?.role,
-        created_at: supabase_user?.identities[0]?.created_at,
-        updated_at: supabase_user?.identities[0]?.updated_at,
-        name: supabase_user?.user_metadata?.user_name,
-      })
-      .throwOnError()
+
+    if (supabase_user && supabase_user.identities && supabase_user.identities.length > 0) {
+      // Check if user already exists in the database
+      const { data: userExists, error: userExistsError } = await supabase
+        .from('users')
+        .select('user_id')
+        .eq('user_id', supabase_user.identities[0].user_id)
+        .single();
+
+      if (userExistsError && !userExists) {
+        // Only insert if user does not exist
+        await supabase
+          .from('users')
+          .insert({
+            user_id: supabase_user.identities[0].user_id,
+            email: supabase_user.email,
+            role: supabase_user.role,
+            created_at: supabase_user.identities[0].created_at,
+            updated_at: supabase_user.identities[0].updated_at,
+            name: supabase_user.user_metadata?.user_name,
+          })
+          .throwOnError();
+      } else {
+        console.log('User already exists, skipping insertion.');
+      }
+    } else {
+      // Handle cases where identities are undefined or empty
+      console.error('No identities found for the user');
+    }
   }
 
-  // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin)
 }
