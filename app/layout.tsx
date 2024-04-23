836c9ad655db52a908bb7c6e99352a354f0f196b
Harshal Ranjhani <harshalranjhani@Harshals-MacBook-Air.local>
2024-04-23 20:57:22 +0530

    fix: font import issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next'
 import { Toaster } from 'react-hot-toast'
 
 import '@/app/globals.css'
-import { fontMono, fontSans } from '@/lib/fonts'
+import { fontMono } from '@/lib/fonts'
 import { cn } from '@/lib/utils'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { Providers } from '@/components/providers'
@@ -37,7 +37,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={cn(
           'font-sans antialiased',
-          fontSans.variable,
+          // fontSans.variable,
           fontMono.variable
         )}
       >
