94b703193ea2e4fdd8e4cbd870939514b6ed3f33
Harshal Ranjhani <harshalranjhani@Harshals-MacBook-Air.local>
2024-05-09 12:09:00 +0530

    send nerd nest welcome email

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -15,17 +15,21 @@ export async function GET(request: Request) {
     const session = await supabase.auth.exchangeCodeForSession(code)
     const supabase_user = session?.data?.session?.user
 
-    if (supabase_user && supabase_user.identities && supabase_user.identities.length > 0) {
+    if (
+      supabase_user &&
+      supabase_user.identities &&
+      supabase_user.identities.length > 0
+    ) {
       // Check if user already exists in the database
       const { data: userExists, error: userExistsError } = await supabase
         .from('users')
         .select('user_id')
         .eq('user_id', supabase_user.identities[0].user_id)
-        .single();
+        .single()
 
       if (userExistsError && !userExists) {
         // Only insert if user does not exist
-        const {data} = await supabase
+        const { data } = await supabase
           .from('users')
           .insert({
             user_id: supabase_user.identities[0].user_id,
@@ -33,16 +37,44 @@ export async function GET(request: Request) {
             role: supabase_user.role,
             created_at: supabase_user.identities[0].created_at,
             updated_at: supabase_user.identities[0].updated_at,
-            name: supabase_user.user_metadata?.user_name,
+            name: supabase_user.user_metadata?.user_name
           })
-          .throwOnError();
-          console.log("User data after signup:",data)
+          .throwOnError()
+        console.log('User data after signup:', data)
+
+        // send email to user
+        const mailObj = {
+          email: supabase_user.email,
+          name: supabase_user.user_metadata?.user_name
+        }
+
+        try {
+          const response = await fetch(
+            `https://harshalranjhaniapi.vercel.app/mail/nerd-nest/welcome`,
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                mailObj
+              })
+            }
+          )
+          if (response.status === 200) {
+            console.log('Email sent successfully')
+          } else {
+            console.log('Failed to send email')
+          }
+        } catch (e: any) {
+          console.error(e.message)
+        }
       } else {
-        console.log('User already exists, skipping insertion.');
+        console.log('User already exists, skipping insertion.')
       }
     } else {
       // Handle cases where identities are undefined or empty
-      console.error('No identities found for the user');
+      console.error('No identities found for the user')
     }
   }
 
